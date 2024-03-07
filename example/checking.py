3efa0ec45463c67f2bfe2caebf164e785605d752
Andorfim <andrey-efimov-2002@mail.ru>
2024-03-07 23:29:42 +0300

    checking

--- example/checking.py
+++ example/checking.py
@@ -1,13 +1,1 @@
-import numpy as np
-
-
-
-u = np.linspace(-100, 100, 100)
-v = np.linspace(-100, 100, 100)
-
-U, V = np.meshgrid(u, v)
-X=U*V
-Y=U
-Z = V*V
-
-x = 0
+# Файл для проверки моих алгоритмов
