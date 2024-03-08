31b5e9901beb87ceb746732e076fc2306f1db56d
Andorfim <andrey-efimov-2002@mail.ru>
2024-03-08 03:27:47 +0300

    Carrier Generation Rate and changed probability function

--- example/check-probability.py
+++ example/check-probability.py
@@ -5,8 +5,8 @@ import numpy as np
 
 
 
-x = np.linspace(1e-8, 1e-6, 10)
-y = np.linspace(1e-8, 5e-7, 10)
+x = np.linspace(1e-8, 1e-6, 100)
+y = np.linspace(1e-8, 5e-7, 100)
 
 
 s_beam = 1e4
@@ -18,7 +18,7 @@ data_x = np.array([])
 data_y = np.array([])
 
 for boundary in x:
-    data_x = np.concatenate((data_x, np.linspace(boundary, boundary, 10)))
+    data_x = np.concatenate((data_x, np.linspace(boundary, boundary, 100)))
     data_y = np.concatenate((data_y, y))
 
 probability_tool = Probability(diffusion_length=diffusion_length, relationship=relationship)
@@ -35,13 +35,13 @@ data_probability = np.array(data_probability)
 data_probability_plot = []
 
 for data in data_probability:
-    data_probability_plot.append(np.linspace(data, data, len(data_probability)))
+    data_probability_plot.append([data])
 
 data_probability = np.array(data_probability_plot)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-surf = ax.plot_surface(data_x, data_y, data_probability)
+surf = ax.scatter(data_x, data_y, data_probability)
 
 
 
--- example/checking.py
+++ example/checking.py
@@ -1,1 +1,5 @@
 # Файл для проверки моих алгоритмов
+
+import numpy as np
+
+print(np.linspace(1, 1, 1))
